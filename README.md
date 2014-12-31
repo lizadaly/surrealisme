ed2c5bbb0b46830d9ec2531c32f178e5969c2cc5
Liza Daly <liza@safaribooksonline.com>
2014-12-31 14:36:28 -0500

    Start with nanogenmo code

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-surrealisme
-===========
+Seraphs: NaNoGenMo2014
+=============
 
-Generate a 20s-style surrealist book
+Generate a 50,000+ word "book" based on the Voynich Manuscript. Uses the following resources:
+
+* The Flickr API and Internet Archive Commons account to grab 18th century images based on specific themes like "biology" and "alchemy."
+* Processes the resulting illustrations to match background colors and discard undesirable images.
+* Uses a randomized corpus of the original Voynich Manuscript (from the EVA transcription) remapped to a public domain Voynich-like font.
+* Generates a set of themed "folios" based on the themes from the original manuscript.
+* Converts the output using CSS3 paged media to print-ready PDF (requires a local installation of Prince XML).
+
+The source code in this repository is in the public domain. Note that a mix of commercial and closed-source software is necessary to generate the final output.
+
+Installation
+------------
+
+Set up a virtual environment:
+
+```
+virtualenv ve
+````
+
+Ensure that you have various JPEG libraries installed (for OS X users, `brew install libjpeg` and `brew install libpng`).
+
+Install the dependencies:
+
+```
+. ve/bin/activate
+python setup.py develop
+```
+
+Get a Flickr API key and add it to a directory called `secret`:
+
+```
+mkdir secret
+cat "FLICKR_KEY = 'YOUR-KEY-HERE'" > secret/__init__.py
+cat "FLICKR_SECRET = 'YOUR-SECRET-HERE'" >> secret/__init__.py
+```
+
+Install PrinceXML (http://princexml.com/) locally.
+
+Run the program:
+
+```
+python seraphs/generate.py
+```
+
+The program should go off and acquire a _lot_ of images from Flickr, randomize them, and generate a PDF somewhere in the 400-page range.
+
+Once it runs, it will cache the resulting images (as local pickle files) in `seraphs/cache` and pull a random set from those. Delete that directory to re-acquire the assets from Flickr.
+
+The output will be `seraphs/build/book.pdf`. It will be _very large_ (more than 1 gigabyte).
+
+Example output
+==============
+
+![Page](examples/page1.png)
+![Page](examples/page2.png)
+![Page](examples/page3.png)
+![Page](examples/page4.png)
+![Page](examples/page5.png)
+![Page](examples/page6.png)
+![Page](examples/page7.png)
+![Page](examples/page8.png)
+![Page](examples/page9.png)
+![Page](examples/page10.png)
+![Page](examples/page11.png)
+![Page](examples/page12.png)
+![Page](examples/page13.png)
+![Page](examples/page14.png)
+![Page](examples/page15.png)
+![Page](examples/page16.png)
+
+
---
+++ seraphs/flickr.py
@@ -0,0 +1,110 @@
+# encoding: utf-8
+# Created by Liza Daly <lizadaly@gmail.com>
+#
+# This work is in the public domain http://creativecommons.org/publicdomain/zero/1.0/
+# Libraries used by this work may be commercial or have other copyright restrictions.
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.propagate = False
+
+from PIL import Image
+from StringIO import StringIO
+
+import colorsys
+import flickrapi
+import os.path
+import random
+import requests
+
+from secret import FLICKR_KEY, FLICKR_SECRET
+from seraphs import BUILD_DIR
+
+IA_METADATA_URL = 'https://archive.org/metadata/{}'
+
+FLICKR_USER_ID = '126377022@N07'  # The Internet Archive's Flickr ID
+MAX_PHOTOS_PER_SECTION = 45
+MIN_LIGHTNESS = 200  # Minimize lightness value of the image's primary (background color)
+MIN_SIZE = 300  # Minimum length or width of the image, in pixels
+
+class BookImage(object):
+    def __init__(self, url, width, height, primary_color, src):
+        self.url = url
+        self.width = width
+        self.height = height
+        self.primary_color = primary_color
+        self.src = src
+
+def flickr_search(text, tags='bookcentury1700,bookcentury1600'):
+    '''Request images from the IA Flickr account with the given century tags and the related text'''
+    book_images = []
+
+    flickr = flickrapi.FlickrAPI(FLICKR_KEY, FLICKR_SECRET, format='etree')
+    photos = flickr.walk(user_id=FLICKR_USER_ID,
+                         per_page=200,
+                         text=text,
+                         tag_mode='any',
+                         tags=tags,
+                         extras='url_o',
+                         sort='interestingness-desc')
+
+    count = 0
+
+    # Randomize the result set
+    photos = list(photos)
+    random.shuffle(photos)
+
+    for photo in photos:
+
+        # Ensure that images are the correct minimize size
+        if int(photo.get('height_o')) < MIN_SIZE or int(photo.get('width_o') < MIN_SIZE):
+            continue
+
+        img_url = photo.get('url_o')
+        img_file = requests.get(img_url, stream=True)
+        img_file.raw.decode_content = True
+        im = Image.open(StringIO(img_file.raw.read()))
+
+        # Main colors
+        colors = max(im.getcolors(im.size[0] * im.size[1]))[1]  # 2nd value in the tuple is the RGB color set
+
+        try:
+            hls = colorsys.rgb_to_hls(colors[0], colors[1], colors[2])
+        except TypeError as te:
+            logging.warn(te)
+            continue
+        except ZeroDivisionError:
+            logging.error("{} {} {}".format(colors[0], colors[1], colors[2]))
+            continue
+
+        lightness = int(hls[1])
+
+        # Skip any images without a light primary color (lazy way of finding background), or those that are too small
+        if lightness < MIN_LIGHTNESS:
+            continue
+
+        img_filename = "{}.png".format(photo.get('id'))
+        img_dir = os.path.join(BUILD_DIR, img_filename)
+
+        if not os.path.exists(BUILD_DIR):
+            os.makedirs(BUILD_DIR)
+
+        im.save(img_dir)
+
+        src = 'https://www.flickr.com/photos/{}/{}'.format(photo.get('owner'), photo.get('id'))
+        book_images.append(BookImage(url=img_filename,
+                                     width=im.size[0],
+                                     height=im.size[1],
+                                     primary_color=colors,
+                                     src=src))
+
+        count += 1
+        if count > MAX_PHOTOS_PER_SECTION:
+            break
+
+    if count < MAX_PHOTOS_PER_SECTION:
+        logging.warn("Did not get enough images for section {}: only {}".format(text, count))
+
+    return book_images
