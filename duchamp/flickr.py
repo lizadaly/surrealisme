d66c51447bd4739155037a9cbf2410aa6c20c04e
Liza Daly <lizadaly@gmail.com>
2015-01-01 17:08:12 -0500

    More sophisticated algorithsm for combining

--- duchamp/flickr.py
+++ duchamp/flickr.py
@@ -13,8 +13,9 @@ requests_log = logging.getLogger("flickrapi.core")
 requests_log.propagate = False
 
 from pprint import pprint
-from PIL import Image, ImageChops, ImageOps
-from cStringIO import StringIO
+from PIL import Image, ImageChops, ImageOps, ImageStat, ImageFilter
+import math
+from StringIO import StringIO
 import numpy as np
 import colorsys
 import flickrapi
@@ -32,9 +33,9 @@ from duchamp import BUILD_DIR, CASCADE_FILE, CACHE_DIR
 IA_METADATA_URL = 'https://archive.org/metadata/{}'
 
 FLICKR_USER_ID = '126377022@N07'  # The Internet Archive's Flickr ID
-MAX_PHOTOS_PER_SECTION = 5
+MAX_PHOTOS_PER_SECTION = 20
 MIN_LIGHTNESS = 200  # Minimize lightness value of the image's primary (background color)
-MIN_SIZE = 300  # Minimum length or width of the image, in pixels
+MIN_SIZE = 1000  # Minimum length or width of the image, in pixels
 
 if not os.path.exists(CACHE_DIR):
     os.makedirs(CACHE_DIR)
@@ -53,7 +54,7 @@ def create_opencv_image_from_stringio(img_stream, cv2_img_flag=0):
     img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
     return cv2.imdecode(img_array, cv2_img_flag)
 
-def flickr_search(text, tags='bookdecade1920'):
+def flickr_search(text, tags=('bookdecade1890','bookdecade1880','bookdecade1900','bookdecade1910')):
     '''Request images from the IA Flickr account with the given century tags and the related text'''
     book_images = []
 
@@ -62,10 +63,10 @@ def flickr_search(text, tags='bookdecade1920'):
     search = flickr.walk(user_id=FLICKR_USER_ID,
                          per_page=200,
                          text=text,
-                         tag_mode='all',
+                         tag_mode='any',
                          tags=tags,
                          extras='url_o',
-                         sort='relevance')
+                         sort='interestingness-desc')
 
     count_faces = 0
     count = 0
@@ -94,7 +95,7 @@ def flickr_search(text, tags='bookdecade1920'):
         small_cv_image = cv2.resize(cv_image, (0,0), fx=0.25, fy=0.25)                 
         faces = face_cascade.detectMultiScale(
             small_cv_image,
-            scaleFactor=1.4,
+            scaleFactor=1.1,
             minNeighbors=4,  # Minimize errors
             minSize=(25, 25),  # 25% of the image has to be a 'face-like' thing
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -102,6 +103,8 @@ def flickr_search(text, tags='bookdecade1920'):
 
         image = BookImage()
         image._cv_image = cv_image
+        img_io.seek(0)
+        image._img_io = img_io.read()
         image._metadata = etree.tostring(photo, encoding='utf8')
 
         # Save off some metadata about it 
@@ -128,62 +131,86 @@ def flickr_search(text, tags='bookdecade1920'):
             photos['non_faces'].append(image)  # It's not a face
             
         if count_faces > MAX_PHOTOS_PER_SECTION:
+            print "Got enough pictures..."
             break
         
-        if count > 10:
-            break
         count += 1
         
     pickle.dump(photos, open(CACHE_FILE, 'w'))
     return photos
 
+def brightness(im):
+   stat = ImageStat.Stat(im)
+   r,g,b = stat.mean
+   return math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
 
 def process_photos(photos):
-    pprint(photos)
+
+    # Randomize
+    random.shuffle(photos['non_faces'])
+    random.shuffle(photos['faces'])
     
     for photo in photos['faces']:
 
         if not os.path.exists(BUILD_DIR):
             os.makedirs(BUILD_DIR)
 
-        # Main colors
-#        try:
-#            d = im.getcolors(im.size[0] * im.size[1])
-#
-#            colors1 = max(d)[1] 
-#            hls1 = colorsys.rgb_to_hls(colors1[0], colors1[1], colors1[2])
-#            lightness1 = int(hls1[1])
+        hist = cv2.calcHist([photo._cv_image],[0],None,[256],[0,256])
+        # Find one that's very different
+        method = cv2.cv.CV_COMP_BHATTACHARYYA
+        
+        for p2 in list(photos['non_faces']):
+            hist2 = cv2.calcHist([p2._cv_image],[0],None,[256],[0,256])
+            # Smaller (0) is more similar than very different (1)
+            difference = cv2.compareHist(hist, hist2, method)
+            if difference > 0.6:
+                print "Difference was {}".format(difference)
+                
+                # Choose this image
+                im = Image.open(StringIO(photo._img_io))
+                im2 = Image.open(StringIO(p2._img_io))
 
-#        except:
-#            continue
+                # Fit the smaller to the larger
+                if im.size[0] + im.size[1] > im2.size[0] + im2.size[1]:
+                    im2 = ImageOps.fit(im2, im.size)
+                else:
+                    im = ImageOps.fit(im, im2.size)
 
-#        im.save(img_dir)
+                try:
+                    brightness1 = brightness(im)
+                    #print "brightness1: {}".format(brightness1)
+                    brightness2 = brightness(im2)
+                    #print "brightness2: {}".format(brightness2)
+                except:
+                    continue
+                if brightness1 < brightness2:
+                    if random.randint(1, 10) < 5:
+                        im = ImageOps.solarize(im)
+                        im = im.filter(ImageFilter.GaussianBlur(2))
+                        im = ImageOps.grayscale(im)
+                        im2 = ImageOps.grayscale(im2)                
+                    im = im.filter(ImageFilter.SMOOTH_MORE)                                        
+                    im2 = ImageOps.invert(im2)
+                    im = ImageChops.screen(im, im2)
+                    img_dir = os.path.join(BUILD_DIR, 'screen-' + photo._img_filename)
 
-#         # Fit the smaller to the larger
-#         if im.size[0] + im.size[1] > im2.size[0] + im2.size[1]:
-#             im2 = ImageOps.fit(im2, im.size)
-#         else:
-#             im = ImageOps.fit(im, im2.size)
+                else:
+                    im = ImageChops.multiply(im, im2)
+                    im = im.filter(ImageFilter.EDGE_ENHANCE_MORE)
+                    im = im.filter(ImageFilter.SMOOTH_MORE)                    
+                    img_dir = os.path.join(BUILD_DIR, 'multiply-' + photo._img_filename)
 
-#         print im.size
-#         print im2.size
-#         # composite the corresponding image from the first set
-#         if lightness1 == lightness2:
-#             im = ImageChops.blend(im, im2, 1)
-#             img_dir = os.path.join(BUILD_DIR, 'blend-' + img_filename)
-#         elif lightness1 > lightness2:
-#             im = ImageChops.subtract(im, im2)
-#             #im = ImageChops.screen(im, im2)
-#             im = ImageOps.solarize(im, 200)            
-#             im = ImageOps.grayscale(im)
-#             img_dir = os.path.join(BUILD_DIR, 'subtract-' + img_filename)            
-#         else:
-#             im = ImageChops.multiply(im, im2)
-#             img_dir = os.path.join(BUILD_DIR, 'multiply-' + img_filename)
+                    
+                photos['non_faces'].remove(p2)
 
-# #        im = ImageOps.grayscale(im)
-#         im = ImageOps.autocontrast(im)
-#         im.save(img_dir)
+                im = ImageOps.autocontrast(im)
+                im.save(img_dir)
+                break
+            
+                # Apply an adaptive histogram transformation to improve the final image
+                #cv2.imshow('normal', photo._cv_image)
+                #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+                #photo._cv_image = clahe.apply(photo._cv_image)
 
 
 if __name__ == '__main__':
@@ -197,7 +224,7 @@ if __name__ == '__main__':
         print "Loading from cache..."
         photos = pickle.load(open(CACHE_FILE))
     else:
-        photos = flickr_search(('woman', ))
+        photos = flickr_search(('female'))
     process_photos(photos)
     
     
